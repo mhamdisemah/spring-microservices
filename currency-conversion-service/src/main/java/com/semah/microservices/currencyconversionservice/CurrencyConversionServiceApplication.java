27ce9b2568dcb679a7c8ecf31308e5feb1d6838c
mhamdi semah <mhamdi.semah@gmail.com>
2018-04-21 13:25:02 +0100

    First commit

---
+++ currency-conversion-service/src/main/java/com/semah/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,24 @@
+package com.semah.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableFeignClients("com.semah.microservices.currencyconversionservice")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+}
